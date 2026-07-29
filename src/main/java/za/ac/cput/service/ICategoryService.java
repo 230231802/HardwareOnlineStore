1aa99e0e6091c32dcaeb3a5e4fca4124f2c7e20f
Sinoxolo05 <230801846@mycput.ac.za>
2026-07-29 17:51:01 +0200

    Added Product and Category controllers and controller test

--- src/main/java/za/ac/cput/service/ICategoryService.java
+++ src/main/java/za/ac/cput/service/ICategoryService.java
@@ -3,14 +3,20 @@ package za.ac.cput.service;
 import za.ac.cput.domain.Category;
 
 import java.util.List;
-import java.util.Set;
 /* ICategoryService.java
    Category Service Interface
    Author: Sinoxolo Kobeni (230801846)
    Date: 12 July 2026 */
 
-public interface ICategoryService extends IService<Category, String> {
+public interface ICategoryService {
+
+    Category create(Category category);
+
+    Category read(String categoryId);
+
+    Category update(Category category);
+
+    boolean delete(String categoryId);
 
     List<Category> getAll();
-
 }
--- src/main/java/za/ac/cput/service/IProductService.java
+++ src/main/java/za/ac/cput/service/IProductService.java
@@ -3,14 +3,20 @@ package za.ac.cput.service;
 import za.ac.cput.domain.Product;
 
 import java.util.List;
-import java.util.Set;
 /* IProductService.java
    Product Service Interface
    Author: Sinoxolo Kobeni (230801846)
    Date: 12 July 2026 */
 
-public interface IProductService extends IService<Product, String> {
+public interface IProductService {
+
+    Product create(Product product);
+
+    Product read(String productId);
+
+    Product update(Product product);
+
+    boolean delete(String productId);
 
     List<Product> getAll();
-
 }
